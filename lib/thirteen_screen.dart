6ce24f6d4ba243a1b031f6834f9a8af18e7fa256
Panchani Urvihsa <urvisha1109@gmail.com>
2023-04-12 14:03:03 +0530

    4/12/2023

---
+++ lib/thirteen_screen.dart
@@ -0,0 +1,211 @@
+import 'package:flutter/material.dart';
+import 'package:pet_animal_app/commen_widget/app_container.dart';
+
+import 'commen_widget/botton_navigationbar_screen.dart';
+
+class ThirteenScreen extends StatefulWidget {
+  const ThirteenScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ThirteenScreen> createState() => _ThirteenScreenState();
+}
+
+class _ThirteenScreenState extends State<ThirteenScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 15,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                children: [
+                  IconButton(
+                    padding: const EdgeInsets.only(right: 25),
+                    icon: const Icon(
+                      Icons.arrow_back_ios_new_outlined,
+                      color: Color(0xffD0AC8A),
+                      size: 20,
+                    ),
+                    onPressed: () {
+                      debugPrint("Twelve Screen--------->");
+                      Navigator.pop(context);
+                    },
+                  ),
+                  const Text(
+                    "Mi mascota",
+                    style: TextStyle(
+                      color: Color(0xff191D21),
+                      fontFamily: "SF Pro",
+                      fontSize: 14,
+                      fontWeight: FontWeight.w400,
+                      fontStyle: FontStyle.normal,
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 30,
+                  ),
+                  const Text(
+                    "Veterinario online",
+                    style: TextStyle(
+                      color: Color(0xff734B3E),
+                      fontFamily: "SF Pro",
+                      fontSize: 16,
+                      fontWeight: FontWeight.w400,
+                      fontStyle: FontStyle.normal,
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              const Padding(
+                padding: EdgeInsets.only(left: 290),
+                child: Text(
+                  "Usuario",
+                  style: TextStyle(
+                    color: Color(0xff734B3E),
+                    fontFamily: "SF Pro",
+                    fontSize: 18,
+                    fontWeight: FontWeight.w700,
+                    fontStyle: FontStyle.normal,
+                  ),
+                ),
+              ),
+              const AppContainer(
+                  text:
+                      "Hola, le escribo porque mi perrita,\nLuli, no se ha sentido muy bien los\núltimos días",
+                  margin: EdgeInsets.only(left: 120, top: 5, bottom: 12),
+                  color: Color(0xffB1CFEB),
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(40),
+                      topRight: Radius.circular(40),
+                      bottomRight: Radius.circular(40))),
+              const AppContainer(
+                  text:
+                      "Tiene mucho vomito. Me gustaría\nsaber si me puede dar algunos\nconsejos",
+                  margin: EdgeInsets.only(left: 120, top: 5, bottom: 12),
+                  color: Color(0xffB1CFEB),
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(40),
+                      topRight: Radius.circular(40),
+                      bottomRight: Radius.circular(40))),
+              const Text(
+                "Cleo",
+                style: TextStyle(
+                  color: Color(0xff734B3E),
+                  fontFamily: "SF Pro",
+                  fontSize: 18,
+                  fontWeight: FontWeight.w700,
+                  fontStyle: FontStyle.normal,
+                ),
+              ),
+              const AppContainer(
+                text:
+                    "¡Hola, Yasmin! Te recomiendo que\nvisites al veterinario, es importante\nexaminarla antes.",
+                margin: EdgeInsets.only(bottom: 5, top: 5),
+              ),
+              const Padding(
+                padding: EdgeInsets.only(left: 290),
+                child: Text(
+                  "Usuario",
+                  style: TextStyle(
+                    color: Color(0xff734B3E),
+                    fontFamily: "SF Pro",
+                    fontSize: 18,
+                    fontWeight: FontWeight.w700,
+                    fontStyle: FontStyle.normal,
+                  ),
+                ),
+              ),
+              const AppContainer(
+                  text: "Ahh y qué día tienen disponible?",
+                  color: Color(0xffB1CFEB),
+                  margin: EdgeInsets.only(left: 110, top: 5, bottom: 12),
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(40),
+                      topRight: Radius.circular(40),
+                      bottomRight: Radius.circular(40))),
+              const Text(
+                "Cleo",
+                style: TextStyle(
+                  color: Color(0xff734B3E),
+                  fontFamily: "SF Pro",
+                  fontSize: 18,
+                  fontWeight: FontWeight.w700,
+                  fontStyle: FontStyle.normal,
+                ),
+              ),
+              const AppContainer(
+                text:
+                    "Te recomiendo que visites este link\ny te aparecerán las ",
+                label: "clínicas más cercanas a tu zona",
+              ),
+              Row(
+                children: [
+                  Container(
+                    padding: const EdgeInsets.only(
+                        top: 15.5, bottom: 15.5, left: 10, right: 68),
+                    decoration: const BoxDecoration(
+                        color: Color(0xffF1F2F4),
+                        borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(16),
+                            bottomLeft: Radius.circular(16))),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: const [
+                        Icon(Icons.wechat_sharp, color: Color(0xff734B3E)),
+                        SizedBox(
+                          width: 15,
+                        ),
+                        Text(
+                          "Escribe un mensaje a Cleo",
+                          style: TextStyle(
+                            color: Color(0xff424C57),
+                            fontFamily: "SF Pro",
+                            fontSize: 16,
+                            fontWeight: FontWeight.w400,
+                            fontStyle: FontStyle.normal,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 14, vertical: 18),
+                    decoration: const BoxDecoration(
+                      borderRadius: BorderRadius.only(
+                        topRight: Radius.circular(
+                          16,
+                        ),
+                        bottomRight: Radius.circular(16),
+                      ),
+                      color: Color(0xffD0AC8A),
+                    ),
+                    child: const Text(
+                      "Enviar",
+                      style: TextStyle(
+                        color: Color(0xff191D21),
+                        fontFamily: "SF Pro",
+                        fontSize: 16,
+                        fontWeight: FontWeight.w400,
+                        fontStyle: FontStyle.normal,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+      bottomNavigationBar: const BottomNavigationBarScreen(),
+    );
+  }
+}
---
+++ lib/twelve_screen.dart
@@ -0,0 +1,225 @@
+import 'package:flutter/material.dart';
+import 'package:pet_animal_app/routes/routes_name.dart';
+
+import 'commen_widget/botton_navigationbar_screen.dart';
+
+class TwelveScreen extends StatefulWidget {
+  const TwelveScreen({Key? key}) : super(key: key);
+
+  @override
+  State<TwelveScreen> createState() => _TwelveScreenState();
+}
+
+class _TwelveScreenState extends State<TwelveScreen> {
+  List<Map> information = [
+    {
+      "image": "assets/images/12_first_dog.png",
+      "text": "¿Cómo proteger a tu mascota del calor?",
+      "label":
+          "Si tu mascota se encuentra en un balcón, terraza, patio\no jardín, debe tener sombra o alguna zona donde..."
+    },
+    {
+      "image": "assets/images/12_second_dog.png",
+      "text": "¿Cómo bañar a tu perro en invierno?",
+      "label": "Regula la temperatura ambiente..."
+    },
+    {
+      "image": "assets/images/12_third_dog.png",
+      "text": "¿Cuanto tiempo debe salir mi perro al dia?",
+      "label":
+          "En general, un can requiere de 30 minutos a 2 horas de\nejercicio diario, repartidos en 3 o 4 paseos cada día..."
+    },
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 15,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                children: [
+                  IconButton(
+                    padding: const EdgeInsets.only(right: 25),
+                    icon: const Icon(
+                      Icons.arrow_back_ios_new_outlined,
+                      color: Color(0xffD0AC8A),
+                      size: 20,
+                    ),
+                    onPressed: () {
+                      debugPrint("Eleven Screen");
+                      Navigator.pop(context);
+                    },
+                  ),
+                  const Text(
+                    "Suscríbete",
+                    style: TextStyle(
+                      color: Color(0xff191D21),
+                      fontFamily: "SF Pro",
+                      fontSize: 14,
+                      fontWeight: FontWeight.w400,
+                      fontStyle: FontStyle.normal,
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 50,
+                  ),
+                  const Text(
+                    "Tips y cuidados",
+                    style: TextStyle(
+                      color: Color(0xff734B3E),
+                      fontFamily: "SF Pro",
+                      fontSize: 16,
+                      fontWeight: FontWeight.w400,
+                      fontStyle: FontStyle.normal,
+                    ),
+                  ),
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.only(
+                  bottom: 30,
+                  top: 30,
+                ),
+                child: Row(
+                  children: [
+                    Container(
+                      padding: const EdgeInsets.only(
+                        left: 10,
+                        top: 8,
+                        bottom: 7,
+                        right: 173,
+                      ),
+                      decoration: const BoxDecoration(
+                          color: Color(0xffF1F2F4),
+                          borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(16),
+                              bottomLeft: Radius.circular(16))),
+                      child: Row(
+                        children: const [
+                          Icon(
+                            Icons.search,
+                            color: Color(0xff734B3E),
+                          ),
+                          SizedBox(
+                            width: 20,
+                          ),
+                          Text(
+                            "Buscar",
+                            style: TextStyle(
+                              color: Color(0xff424C57),
+                              fontFamily: "SF Pro",
+                              fontSize: 16,
+                              fontWeight: FontWeight.w400,
+                              fontStyle: FontStyle.normal,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 10, vertical: 10),
+                      decoration: const BoxDecoration(
+                        borderRadius: BorderRadius.only(
+                          topRight: Radius.circular(
+                            16,
+                          ),
+                          bottomRight: Radius.circular(16),
+                        ),
+                        color: Color(0xffD0AC8A),
+                      ),
+                      child: const Text(
+                        "Filtrar por:",
+                        style: TextStyle(
+                          color: Color(0xff191D21),
+                          fontFamily: "SF Pro",
+                          fontSize: 16,
+                          fontWeight: FontWeight.w400,
+                          fontStyle: FontStyle.normal,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 536,
+                child: ListView.separated(
+                  itemBuilder: (context, index) => InkWell(
+                    onTap: () {
+                      if (index == 2) {
+                        Navigator.pushNamed(context, RoutesName.thirteenScreen);
+                      }
+                      debugPrint("Thirteen Screen-------->");
+                    },
+                    child: Container(
+                      height: 230,
+                      decoration: const BoxDecoration(
+                        boxShadow: [
+                          BoxShadow(
+                              color: Color(0x1a191d21),
+                              blurRadius: 16,
+                              offset: Offset(0, 2),
+                              blurStyle: BlurStyle.outer)
+                        ],
+                        borderRadius: BorderRadius.all(Radius.circular(24)),
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          ClipRRect(
+                              borderRadius: const BorderRadius.only(
+                                  topRight: Radius.circular(24),
+                                  topLeft: Radius.circular(24)),
+                              child: Image.asset(information[index]["image"])),
+                          Padding(
+                            padding: const EdgeInsets.only(left: 15, top: 10),
+                            child: Text(
+                              information[index]["text"],
+                              style: const TextStyle(
+                                color: Color(0xff191D21),
+                                fontStyle: FontStyle.normal,
+                                fontSize: 16,
+                                fontWeight: FontWeight.w400,
+                                fontFamily: "SF Pro",
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(left: 15),
+                            child: Text(
+                              information[index]["label"],
+                              style: const TextStyle(
+                                color: Color(0xff191D21),
+                                fontStyle: FontStyle.normal,
+                                fontSize: 14,
+                                fontWeight: FontWeight.w400,
+                                fontFamily: "SF Pro",
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                  separatorBuilder: (context, index) => const SizedBox(
+                    height: 25,
+                  ),
+                  itemCount: information.length,
+                  physics: const BouncingScrollPhysics(),
+                  shrinkWrap: true,
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+      bottomNavigationBar: const BottomNavigationBarScreen(),
+    );
+  }
+}
